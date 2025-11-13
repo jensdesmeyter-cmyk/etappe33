9a41e61f3dd3734da740e7037e5eb7848404aac3
Jens De Smeyter <jensdesmeyter@gmail.com>
2025-11-13 18:24:25 +0100

    FIX build error

--- next-intl.config.ts
+++ next-intl.config.ts
@@ -1,8 +1,13 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./messages/${locale}.json`)).default
-}));
+export default getRequestConfig(async ({ locale }) => {
+  const messages = (await import(`./messages/${locale}.json`)).default;
+  
+  return {
+    locale,       // <-- add this
+    messages      // <-- keep this
+  };
+});
 
 
 
